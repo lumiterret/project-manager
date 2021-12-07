74a5f66053d1e2d2df9a6f05191052e25312f232
toyrik <toyrik@gmail.com>
2021-12-07 21:04:38 +0300

    Added confirm token and wait status

---
+++ manager/src/Model/User/Service/ConfirmTokenizer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model\User\Service;
+
+use Ramsey\Uuid\Uuid;
+
+class ConfirmTokenizer
+{
+    public function generate(): string
+    {
+        return Uuid::uuid4()->toString();
+    }
+}
