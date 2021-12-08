6a37ce6b503dd87ff0ec67721131b03abe1818a2
toyrik <toyrik@gmail.com>
2021-12-08 12:14:10 +0300

    Extracted user builder

--- manager/tests/Unit/Model/User/Entity/User/Network/AuthTest.php
+++ manager/tests/Unit/Model/User/Entity/User/Network/AuthTest.php
@@ -2,19 +2,15 @@
 
 namespace App\Tests\Unit\Model\User\Entity\User\Network;
 
-use App\Model\User\Entity\User\Id;
 use App\Model\User\Entity\User\Network;
-use App\Model\User\Entity\User\User;
+use App\Tests\Builder\User\UserBuilder;
 use PHPUnit\Framework\TestCase;
 
 class AuthTest extends TestCase
 {
     public function testSuccess(): void
     {
-        $user = new User(
-            Id::next(),
-            new \DateTimeImmutable()
-        );
+        $user = (new UserBuilder())->build();
 
         $user->signUpByNetwork(
             $network = 'vk',
@@ -31,10 +27,7 @@ class AuthTest extends TestCase
 
     public function testAlready(): void
     {
-        $user = new User(
-            $id = Id::next(),
-            $date = new \DateTimeImmutable()
-        );
+        $user = (new UserBuilder())->build();
 
         $user->signUpByNetwork(
             $network = 'vk',
