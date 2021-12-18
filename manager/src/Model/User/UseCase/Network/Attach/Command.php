d17799a6ece2958598d248b29b6787a74c6e8793
toyrik <toyrik@gmail.com>
2021-12-18 08:40:55 +0300

    Added profile network attaching

---
+++ manager/src/Model/User/UseCase/Network/Attach/Command.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model\User\UseCase\Network\Attach;
+
+class Command
+{
+    /**
+     * @var string
+     */
+    public $user;
+    /**
+     * @var string
+     */
+    public $network;
+    /**
+     * @var string
+     */
+    public $identity;
+
+    public function __construct(string $user, string $network, string $identity)
+    {
+        $this->user = $user;
+        $this->network = $network;
+        $this->identity = $identity;
+    }
+}
