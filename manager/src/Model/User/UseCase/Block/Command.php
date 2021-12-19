27ce3625d3283e549fb3a2041537d699b12fef0d
toyrik <toyrik@gmail.com>
2021-12-19 15:26:03 +0300

    Added block and activate buttons

---
+++ manager/src/Model/User/UseCase/Block/Command.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model\User\UseCase\Block;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+}
