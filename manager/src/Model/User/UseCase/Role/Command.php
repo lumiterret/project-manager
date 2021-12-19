af7dba1a8f3f299c0fdccfb3ba9bc2ea8ca67d6b
toyrik <toyrik@gmail.com>
2021-12-19 13:49:16 +0300

    Added user role page

--- manager/src/Model/User/UseCase/Role/Command.php
+++ manager/src/Model/User/UseCase/Role/Command.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\User\UseCase\Role;
 
+use App\Model\User\Entity\User\User;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class Command
@@ -18,5 +19,12 @@ class Command
     public function __construct(string $id)
     {
         $this->id = $id;
+    }
+
+    public static function fromUser(User $user): self
+    {
+        $command = new self($user->getId()->getValue());
+        $command->role = $user->getRole()->getName();
+        return $command;
     }
 }
