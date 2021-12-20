f89c3c4b2aacad25f7a2a3205ee22addf9409000
toyrik <toyrik@gmail.com>
2021-12-20 09:47:02 +0300

    Changed user fetcher

--- manager/src/Model/User/UseCase/Name/Command.php
+++ manager/src/Model/User/UseCase/Name/Command.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\User\UseCase\Name;
 
+use App\Model\User\Entity\User\User;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class Command
@@ -25,5 +26,13 @@ class Command
     public function __construct(string $id)
     {
         $this->id = $id;
+    }
+
+    public static function fromUser(User $user): self
+    {
+        $command = new self($user->getId()->getValue());
+        $command->firstName = $user->getName()->getFirst();
+        $command->lastName = $user->getName()->getLast();
+        return $command;
     }
 }
--- manager/src/ReadModel/User/NetworkView.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\ReadModel\User;
-
-class NetworkView
-{
-    public $network;
-    public $identity;
-}
