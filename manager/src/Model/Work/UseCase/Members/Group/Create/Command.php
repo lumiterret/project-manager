2368f4f105bee53265c60fc29d7573530193624a
toyrik <toyrik@gmail.com>
2021-12-20 18:56:07 +0300

    Added work member groups

---
+++ manager/src/Model/Work/UseCase/Members/Group/Create/Command.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model\Work\UseCase\Members\Group\Create;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    public $name;
+}
