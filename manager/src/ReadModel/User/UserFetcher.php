ba48463c171e5de3071cce0a82c610f33db19ca8
toyrik <toyrik@gmail.com>
2021-12-21 08:29:00 +0300

    Removed unused code

--- manager/src/ReadModel/User/UserFetcher.php
+++ manager/src/ReadModel/User/UserFetcher.php
@@ -32,7 +32,7 @@ class UserFetcher
                 ->from('user_users')
                 ->where('reset_token_token = :token')
                 ->setParameter(':token', $token)
-                ->execute()->fetchColumn(0) > 0;
+                ->execute()->fetchColumn() > 0;
     }
 
     public function findForAuthByEmail(string $email): ?AuthView
