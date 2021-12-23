156db3712b14d97853df2b4599feb8722147fe3e
toyrik <toyrik@gmail.com>
2021-12-23 11:08:05 +0300

    Added project contollers precommit

---
+++ manager/src/ReadModel/Work/Projects/Project/Filter/Filter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\ReadModel\Work\Projects\Project\Filter;
+
+use App\Model\Work\Entity\Members\Member\Status;
+
+class Filter
+{
+    public $name;
+    public $status = Status::ACTIVE;
+}
