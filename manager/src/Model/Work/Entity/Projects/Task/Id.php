d1816ae3a01cd378f931ba2526047e152787260b
toyrik <toyrik@gmail.com>
2021-12-26 08:47:09 +0300

    Added task create

---
+++ manager/src/Model/Work/Entity/Projects/Task/Id.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model\Work\Entity\Projects\Task;
+
+use Webmozart\Assert\Assert;
+
+class Id
+{
+    private $value;
+
+    public function __construct(int $value)
+    {
+        Assert::notEmpty($value);
+        $this->value = $value;
+    }
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return (string)$this->value;
+    }
+}
