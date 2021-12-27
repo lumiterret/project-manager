b89c530305112827b0b3c8574883f52f551f6fe8
toyrik <toyrik@gmail.com>
2021-12-27 09:01:28 +0300

    Added task executors usecases

---
+++ manager/src/Model/Work/UseCase/Projects/Task/Executor/Assign/Command.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model\Work\UseCase\Projects\Task\Executor\Assign;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+    /**
+     * @var array
+     * @Assert\NotBlank()
+     */
+    public $members;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+}
---
+++ manager/src/Model/Work/UseCase/Projects/Task/Executor/Revoke/Command.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model\Work\UseCase\Projects\Task\Executor\Revoke;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+    /**
+     * @Assert\NotBlank()
+     */
+    public $member;
+
+    public function __construct(int $id, string $member)
+    {
+        $this->id = $id;
+        $this->member = $member;
+    }
+}
