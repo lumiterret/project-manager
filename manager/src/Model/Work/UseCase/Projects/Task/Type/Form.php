fb429685fe002708f09c7f775cadd97b46c1a8aa
toyrik <toyrik@gmail.com>
2021-12-27 10:30:44 +0300

    Added form prefixes

--- manager/src/Model/Work/UseCase/Projects/Task/Type/Form.php
+++ manager/src/Model/Work/UseCase/Projects/Task/Type/Form.php
@@ -26,4 +26,9 @@ class Form extends AbstractType
             'data_class' => Command::class,
         ));
     }
+
+    public function getBlockPrefix(): string
+    {
+        return 'type';
+    }
 }
