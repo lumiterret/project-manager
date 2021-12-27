912b0875aff5fc0c645ab02023660129b3d71940
toyrik <toyrik@gmail.com>
2021-12-27 08:43:02 +0300

    Added task parent usecase

---
+++ manager/src/Model/Work/UseCase/Projects/Task/ChildOf/Command.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Model\Work\UseCase\Projects\Task\ChildOf;
+
+use App\Model\Work\Entity\Projects\Task\Task;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+
+    public $parent;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+    public static function fromTask(Task $task): self
+    {
+        $command = new self($task->getId()->getValue());
+        $command->parent = $task->getParent() ? $task->getParent()->getId()->getValue() : null;
+        return $command;
+    }
+}
