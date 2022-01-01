212bb7fe00743511fe85593cc6e904cdcc3351e8
toyrik <toyrik@gmail.com>
2022-01-01 16:15:48 +0300

    Added comments

---
+++ manager/src/Model/Comment/Entity/Comment/IdType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model\Comment\Entity\Comment;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\GuidType;
+
+class IdType extends GuidType
+{
+    public const NAME = 'comment_comment_id';
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value instanceof Id ? $value->getValue() : $value;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return !empty($value) ? new Id($value) : null;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform) : bool
+    {
+        return true;
+    }
+}
---
+++ manager/src/ReadModel/Comment/CommentRow.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\ReadModel\Comment;
+
+class CommentRow
+{
+    public $id;
+    public $date;
+    public $author_id;
+    public $author_name;
+    public $author_email;
+    public $text;
+}
