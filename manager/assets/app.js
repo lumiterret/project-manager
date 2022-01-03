2686804eacd391d81115c4cdf71da266c54bce00
toyrik <toyrik@gmail.com>
2022-01-03 09:37:10 +0300

    Added toastr library

--- manager/assets/app.js
+++ manager/assets/app.js
@@ -14,12 +14,13 @@ import './bootstrap';
 require('@coreui/coreui');
 
 const Centrifuge = require('centrifuge');
+const toastr = require('toastr');
 
 document.addEventListener('DOMContentLoaded', function () {
     let url = document.querySelector('meta[name=centrifugo-url]').getAttribute('content');
     let centrifuge = new Centrifuge(url);
     centrifuge.subscribe('alerts', function (message) {
-        console.log(message.data.message);
+        toastr.info(message.data.message);
     });
     centrifuge.connect();
 });
